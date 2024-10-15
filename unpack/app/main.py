3385db61031b6e737ab817f1ff4643db5e139bff
JoaoHFerreira <jaycops@gmail.com>
2024-10-15 11:36:17 +0100

    lambda image

--- unpack/app/main.py
+++ unpack/app/main.py
@@ -1,22 +1,12 @@
-# from fastapi import FastAPI
-# from mangum import Mangum
+from fastapi import FastAPI
+from mangum import Mangum
 
-# app = FastAPI()
+app = FastAPI()
 
-# # Your routes here
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+# Your routes here
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-# # Create the Mangum adapter
-# lambda_handler = Mangum(app)
-import json
-
-def lambda_handler(event, context):
-    name = event.get("queryStringParameters", {}).get("name", "World")
-    message = f"Hello, {name}!"
-    
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": message})
-    }
+# Create the Mangum adapter
+lambda_handler = Mangum(app)
